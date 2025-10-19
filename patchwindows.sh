349f438e292ff50c1b66893f7adef471306f3557
GH-X <GH-X@users.noreply.github.com>
2025-10-19 18:05:45 +0800

    separate: build commit b7021afff6

--- patchwindows.sh
+++ patchwindows.sh
@@ -3,17 +3,22 @@
 FORGEJO_LENV="${GITHUB_WORKSPACE}/forgejo.env"
 touch "$FORGEJO_LENV"
 
+upatch="update.patch"
+tpatch="translations_zh_CN.patch"
+[ -e "${GITHUB_WORKSPACE}/patches/${FORGEJO_REF}.update.patch" ] && upatch="${FORGEJO_REF}.update.patch"
+[ -e "${GITHUB_WORKSPACE}/patches/${FORGEJO_REF}.translations_zh_CN.patch" ] && tpatch="${FORGEJO_REF}.translations_zh_CN.patch"
+
 cd ./eden
 
 # eden-nightly patch
-if ! patch -p1 < ../patches/update.patch; then
-	echo "FORGEJO_ERROR_UP=!!! update.patch mismatch !!!" >> "$FORGEJO_LENV"
+if ! patch -p1 < ../patches/$upatch; then
+	echo "FORGEJO_ERROR_UP=!!! $upatch mismatch !!!" >> "$FORGEJO_LENV"
 else
 	echo "FORGEJO_ERROR_UP=" >> "$FORGEJO_LENV"
 fi
 # translations zh_CN
-if ! patch -p1 < ../patches/translations_zh_CN.patch; then
-	echo "FORGEJO_ERROR_TP=!!! translations_zh_CN.patch mismatch !!!" >> "$FORGEJO_LENV"
+if ! patch -p1 < ../patches/$tpatch; then
+	echo "FORGEJO_ERROR_TP=!!! $tpatch mismatch !!!" >> "$FORGEJO_LENV"
 else
 	echo "FORGEJO_ERROR_TP=" >> "$FORGEJO_LENV"
 fi
