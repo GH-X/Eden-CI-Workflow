fb84c4fe0bc920b2ecac218e0c7a0b3c5ce15209
GH-X <GH-X@users.noreply.github.com>
2025-10-19 12:14:51 +0800

    separate: Merge 'Eden-CI:master'

--- patchwindows.sh
+++ patchwindows.sh
@@ -1,17 +1,19 @@
 #!/bin/sh -ex
 
-FORGEJO_LENV=${FORGEJO_LENV:-"forgejo.env"}
+FORGEJO_LENV="${GITHUB_WORKSPACE}/forgejo.env"
 touch "$FORGEJO_LENV"
-echo "FORGEJO_ERROR_UP=" >> "$FORGEJO_LENV"
-echo "FORGEJO_ERROR_TP=" >> "$FORGEJO_LENV"
 
 cd ./eden
 
 # eden-nightly patch
 if ! patch -p1 < ../patches/update.patch; then
 	echo "FORGEJO_ERROR_UP=!!! update.patch mismatch !!!" >> "$FORGEJO_LENV"
+else
+	echo "FORGEJO_ERROR_UP=" >> "$FORGEJO_LENV"
 fi
 # translations zh_CN
 if ! patch -p1 < ../patches/translations_zh_CN.patch; then
 	echo "FORGEJO_ERROR_TP=!!! translations_zh_CN.patch mismatch !!!" >> "$FORGEJO_LENV"
+else
+	echo "FORGEJO_ERROR_TP=" >> "$FORGEJO_LENV"
 fi
