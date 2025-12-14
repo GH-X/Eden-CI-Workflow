463362572641e3fb615e91455648c1112aa539c7
GH-X <GH-X@users.noreply.github.com>
2025-12-14 23:29:53 +0800

    Nightly: Add temporary patch

--- patchwindows.sh
+++ patchwindows.sh
@@ -1,10 +1,17 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 FORGEJO_LENV="${GITHUB_WORKSPACE}/forgejo.env"
 touch "$FORGEJO_LENV"
 
 cd ./eden
 
+# temporary
+temporarypatchname="temporary.patch"
+for temporarypatch in $(ls -A ../patches/*$temporarypatchname | sort -n -r); do
+    if patch -p1 < $temporarypatch; then
+        echo "patch $temporarypatch OK !!!"
+    fi
+done
 # translations zh_CN
 tdname="translations_zh_CN.patch"
 tperror="- Patch Error: !!! *$tdname mismatch !!!"
