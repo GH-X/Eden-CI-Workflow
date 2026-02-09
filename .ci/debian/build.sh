03808f87d1b338f098deda9044a2f90c35987f9a
Caio Oliveira <caiooliveirafarias0@gmail.com>
2026-02-09 12:30:19 -0300

    debian: more deep rework (#112)
    * debian: more deep rework
    
    * [chore] Remove GITHUB_WORKSPACE ref
    
    * allow this script to be ran locally
    
    * debian: fix mv from "fakeroot" to host
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>
    
    * freebsd: do it like makedeb just in case
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>
    
    * debian: Ah my the mkdir -p, haven’t used that since the heian era
    
    * build.sh: Remove bashisms
    
    ---------
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- .ci/debian/build.sh
+++ .ci/debian/build.sh
@@ -3,40 +3,54 @@
 # SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-export ROOTDIR="$PWD"
-WORKFLOW_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+ROOTDIR="$PWD"
+BUILDDIR="${BUILDDIR:-$ROOTDIR/build}"
+ARTIFACTS_DIR="$ROOTDIR/artifacts"
 
-GITHUB_WORKSPACE="$ROOTDIR"
+# shellcheck disable=SC1091
+WORKFLOW_DIR=$(CDPATH='' cd -P -- "$(dirname -- "$0")/../.." && pwd)
+. "$WORKFLOW_DIR/.ci/common/project.sh"
 
-# install makedeb
-echo "-- Installing makedeb..."
-[ ! -d makedeb-src ] && git clone 'https://github.com/makedeb/makedeb' makedeb-src
-cd makedeb-src
-git checkout stable
+if ! command -v makedeb > /dev/null 2>&1 ; then
+	# install makedeb
+	echo "-- Installing makedeb..."
+	[ ! -d "$ROOTDIR/makedeb-src" ] && git clone 'https://github.com/makedeb/makedeb' "$ROOTDIR/makedeb-src"
+	cd "$ROOTDIR/makedeb-src"
+	git checkout stable
 
-make prepare VERSION=16.0.0 RELEASE=stable TARGET=apt CURRENT_VERSION=16.0.0 FILESYSTEM_PREFIX="$ROOTDIR/makedeb"
-make
-make package DESTDIR="$ROOTDIR/makedeb" TARGET=apt
+	make prepare VERSION=16.0.0 RELEASE=stable TARGET=apt CURRENT_VERSION=16.0.0 FILESYSTEM_PREFIX="$ROOTDIR/makedeb"
+	make
+	make package DESTDIR="$ROOTDIR/makedeb" TARGET=apt
 
-export PATH="$ROOTDIR/makedeb/usr/bin:$PATH"
+	export PATH="$ROOTDIR/makedeb/usr/bin:$PATH"
+fi
 
 # now build
 echo "-- Building..."
 cd "$ROOTDIR"
 
-SRC="$WORKFLOW_DIR/.ci/debian/PKGBUILD.in"
-DEST=PKGBUILD
+CONFIG_OPTS=""
+if [ -n "${SCCACHE_PATH-}" ] && [ -e "$SCCACHE_PATH" ]; then
+    CONFIG_OPTS=" -DCCACHE_PATH=\"${SCCACHE_PATH}\""
+fi
 
-TAG=$(cat "$GITHUB_WORKSPACE"/GIT-TAG | sed 's/.git//' | sed 's/v//' | sed 's/[-_]/./g' | tr -d '\n')
-if [ -f "$GITHUB_WORKSPACE"/GIT-RELEASE ]; then
+SRC="$WORKFLOW_DIR/.ci/debian/PKGBUILD.in"
+DEST="$ROOTDIR/PKGBUILD"
+
+TAG=$(cat "$ROOTDIR"/GIT-TAG | sed 's/.git//' | sed 's/v//' | sed 's/[-_]/./g' | tr -d '\n')
+if [ -f "$ROOTDIR"/GIT-RELEASE ]; then
 	PKGVER="$TAG"
 else
-	REF=$(cat "$GITHUB_WORKSPACE"/GIT-COMMIT)
+	REF=$(cat "$ROOTDIR"/GIT-COMMIT)
 	PKGVER="$TAG.$REF"
 fi
 
-sed "s/%PKGVER%/$PKGVER/" "$SRC"  > $DEST.1
-sed "s/%ARCH%/$ARCH/"     $DEST.1 > $DEST
+sed "s|%PKGVER%|$PKGVER|"             "$SRC"    > "$DEST.1"
+sed "s|%ARCH%|$ARCH|"                 "$DEST.1" > "$DEST.2"
+sed "s|%WORKFLOWDIR%|$WORKFLOW_DIR/|" "$DEST.2" > "$DEST.3"
+sed "s|%BUILDDIR%|$BUILDDIR|"         "$DEST.3" > "$DEST.4"
+sed "s|%CONFIG_OPTS%|$CONFIG_OPTS|"   "$DEST.4" > "$DEST.5"
+sed "s|%SOURCE%|$ROOTDIR|"            "$DEST.5" > "$DEST"
 
 rm $DEST.*
 
@@ -44,10 +58,10 @@ if ! command -v sudo >/dev/null 2>&1 ; then
 	alias sudo="su - root -c"
 fi
 
-makedeb --print-srcinfo > .SRCINFO
+makedeb --print-srcinfo > "$ROOTDIR/.SRCINFO"
 makedeb -s --no-confirm
 
 # for some grand reason, makepkg does not exit on errors
 ls ./*.deb || exit 1
-
-mv ./*.deb "${PROJECT_PRETTYNAME}-${DEB_NAME}-${ARTIFACT_REF}-${ARCH}.deb"
+mkdir -p "$ARTIFACTS_DIR"
+mv ./*.deb "$ARTIFACTS_DIR/${PROJECT_PRETTYNAME}-${DEB_NAME}-${ARTIFACT_REF}-${ARCH}.deb"
--- .ci/debian/makedeb.sh
+++ .ci/debian/makedeb.sh
@@ -6,6 +6,7 @@ ROOTDIR="$PWD"
 BUILDDIR="/build"
 BUILDUSER="build"
 WORKFLOW_DIR=$(CDPATH='' cd -P -- "$(dirname -- "$0")/../.." && pwd)
+ARTIFACTS_DIR="$ROOTDIR/artifacts"
 
 # Use sudo if available, otherwise run directly
 if command -v sudo >/dev/null 2>&1 ; then
@@ -25,20 +26,22 @@ if ! command -v sudo > /dev/null 2>&1 ; then
 	sudo useradd -m -s /bin/bash -d "$BUILDDIR" "$BUILDUSER"
 	echo "$BUILDUSER ALL=NOPASSWD: ALL" >> /etc/sudoers
 
-	# copy workspace stuff over
+	# copy workspace stuff to fakeroot
 	cp -r "$ROOTDIR/"* "$ROOTDIR/.patch" "$ROOTDIR/.ci" "$ROOTDIR/.reuse" "$BUILDDIR"
 	if [ -d "$ROOTDIR/.cache" ]; then
 		cp -r "$ROOTDIR/.cache" "$BUILDDIR"
-		rm -rf "$ROOTDIR/.cache"
 		chown -R "$BUILDUSER:$BUILDUSER" "$BUILDDIR/.cache"
+		rm -rf "$ROOTDIR/.cache"
 	fi
 	chown -R "$BUILDUSER:$BUILDUSER" "$BUILDDIR/"* "$BUILDDIR/.patch" "$BUILDDIR/.ci" "$BUILDDIR/.reuse"
 
 	cd "$BUILDDIR"
 	sudo -E -u "$BUILDUSER" "$BUILDDIR/.ci/debian/build.sh"
 
+	# copy back from fakeroot to workspace
 	mv "$BUILDDIR/.cache" "$ROOTDIR"
-	cp "$BUILDDIR/"*.deb "$ROOTDIR"
+	mkdir -p "$ARTIFACTS_DIR"
+	cp "$BUILDDIR/artifacts/"*.deb "$ARTIFACTS_DIR"
 # otherwise just run normally
 else
 	"$WORKFLOW_DIR/.ci/debian/build.sh"
