7d9b92b099700ff4b0dc811ce7a2d61d9766c74d
GH-X <GH-X@users.noreply.github.com>
2026-03-03 17:05:44 +0800

    update -28162#5edcdea78f#3074.temporary.patch

--- patchwindows.sh
+++ patchwindows.sh
@@ -5,31 +5,34 @@ touch "$FORGEJO_LENV"
 
 cd ./eden
 
-# temporary
 temppatcherror=""
-temppatchname="temporary.patch"
-for temppatch in $(ls -A ../patches/*$temppatchname | sort -n -r); do
-    if patch -p1 < $temppatch; then
-        echo "patch $temppatch OK !!!"
-        continue
-    fi
-    if ! patch -Rp1 < $temppatch; then
-        temppatcherror="- Patch Error: !!! $temppatchname mismatch !!!"
-        echo "patch $temppatch mismatch !!!"
-    fi
-done
+zhcnpatcherror=""
+temppatch="temporary.patch"
+zhcnpatch="translations_zh_CN.patch"
+apply_patch() {
+	for patchname in $(ls -A ../patches/*$2$1 | sort -n -r); do
+		if patch -p1 < $patchname; then
+			echo "patch $patchname OK !!!"
+			break
+		fi
+		if ! patch -Rp1 < $patchname; then
+			if [ "$1" = "$temppatch" ]; then
+				temppatcherror="- Patch Error: !!! $1 mismatch !!!"
+			fi
+			if [ "$1" = "$zhcnpatch" ]; then
+				zhcnpatcherror="- Patch Error: !!! $1 mismatch !!!"
+			fi
+			echo "patch $patchname mismatch !!!"
+		fi
+	done
+}
+# temporary
+apply_patch $temppatch '#settings.'
+apply_patch $temppatch '#lowmemorydevice.'
+apply_patch $temppatch '#FramePacingMode.'
+apply_patch $temppatch '-28162#5edcdea78f#3074.'
+apply_patch $temppatch '-28093#363d861011#3156.'
 echo "FORGEJO_ERROR_TEMP=$temppatcherror" >> "$FORGEJO_LENV"
 # translations zh_CN
-zhcnpatcherror=""
-zhcnpatchname="translations_zh_CN.patch"
-for zhcnpatch in $(ls -A ../patches/*$zhcnpatchname | sort -n -r); do
-    if patch -p1 < $zhcnpatch; then
-        echo "patch $zhcnpatch OK !!!"
-        break
-    fi
-    if ! patch -Rp1 < $zhcnpatch; then
-        zhcnpatcherror="- Patch Error: !!! $zhcnpatchname mismatch !!!"
-        echo "patch $zhcnpatch mismatch !!!"
-    fi
-done
+apply_patch $zhcnpatch '.'
 echo "FORGEJO_ERROR_ZHCN=$zhcnpatcherror" >> "$FORGEJO_LENV"
