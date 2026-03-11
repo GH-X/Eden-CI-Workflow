40242780184c9fa27e950e0b5408698d6549f1ac
GH-X <GH-X@users.noreply.github.com>
2026-03-12 00:32:05 +0800

    Rename patch

--- patchwindows.sh
+++ patchwindows.sh
@@ -5,34 +5,31 @@ touch "$FORGEJO_LENV"
 
 cd ./eden
 
-temppatcherror=""
-zhcnpatcherror=""
-temppatch="temporary.patch"
-zhcnpatch="translations_zh_CN.patch"
+patcherror=""
 apply_patch() {
-	for patchname in $(ls -A ../patches/*$2$1 | sort -n -r); do
+	for patchname in $(ls -A ../patches/$2*$1 | sort -n -r); do
+		echo "----------[ $patchname ]----------"
 		if patch -p1 < $patchname; then
 			echo "patch $patchname OK !!!"
 			break
 		fi
 		if ! patch -Rp1 < $patchname; then
-			if [ "$1" = "$temppatch" ]; then
-				temppatcherror="- Patch Error: !!! $1 mismatch !!!"
-			fi
-			if [ "$1" = "$zhcnpatch" ]; then
-				zhcnpatcherror="- Patch Error: !!! $1 mismatch !!!"
-			fi
+			patcherror="- Patch Error: !!! $1 mismatch !!!"
 			echo "patch $patchname mismatch !!!"
 		fi
 	done
 }
 # temporary
-apply_patch $temppatch '#settings.'
-apply_patch $temppatch '#FramePacingMode.'
-apply_patch $temppatch '#lowmemorydevice.'
-apply_patch $temppatch '-28162#5edcdea78f#3074.'
-apply_patch $temppatch '-28093#363d861011#3156.'
-echo "FORGEJO_ERROR_TEMP=$temppatcherror" >> "$FORGEJO_LENV"
+temppatchs="temporary.patch"
+previouspatchs=""
+for currentpatchs in $(ls -A ../patches/*$temppatchs | sort -n -r | awk -F\/ '{print $3}' | awk -F\. '{print $1}'); do
+	[ "$currentpatchs" != "" ] || break
+	[ "$currentpatchs" != "$previouspatchs" ] || continue
+	apply_patch $temppatchs $currentpatchs
+	previouspatchs="$currentpatchs"
+done
+echo "FORGEJO_ERROR_TEMP=$patcherror" >> "$FORGEJO_LENV"
 # translations zh_CN
-apply_patch $zhcnpatch '.'
-echo "FORGEJO_ERROR_ZHCN=$zhcnpatcherror" >> "$FORGEJO_LENV"
+patcherror=""
+apply_patch 'translations.patch' 'zh_CN'
+echo "FORGEJO_ERROR_ZHCN=$patcherror" >> "$FORGEJO_LENV"
